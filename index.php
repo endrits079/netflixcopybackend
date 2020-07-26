9b2e6b238abd2e9de74112119cb9b3546bdae198
Endrit Shabani <endrits079@gmail.com>
2020-07-26 20:04:57 +0200

    return logged user

--- index.php
+++ index.php
@@ -46,16 +46,19 @@ if(isset($_POST['register'])){
 if(isset($_POST['login'])){
     $email =FormSanitizer::sanitizeEmail($_POST['email']);
     $password =FormSanitizer::sanitizePassword($_POST['password']);
-    $query = "SELECT * from users WHERE email='$email' AND password='$password'";
+    $query = "SELECT * from users WHERE email='$email' OR username='$email' AND password='$password'";
     $result=mysqli_query($connect,$query);
     $result= mysqli_fetch_assoc($result);
     if(empty($result)){
         $message='Incorrect username or password';
     }
     else {
-        $message = $result['id_user'];
+        extract($result);
+        $message = array('id'=>$id_user,'username'=>$username,'user_type'=>$user_type);
+        $message = json_encode($message);
         $succeed=true;
     }
+
     array_push($array,$message,$succeed);
     echo json_encode($array);
 }
