4db519375e5f723a4986dab17babd18a836f42c0
Endrit Shabani <endrits079@gmail.com>
2020-07-27 22:54:26 +0200

    .gitignore is now working

--- includes/classes/CategoryContainers.php
+++ includes/classes/CategoryContainers.php
@@ -6,17 +6,36 @@ class CategoryContainers{
   }
 
     public function showAllCategories(){
-        $myarray = array();
         $query = "SELECT * FROM categories";
         $result = mysqli_query($this->connect,$query);
-        while($row=mysqli_fetch_assoc($result))
-{
-    extract($row);
-
-    array_push($myarray,array("id"=>$id,"name"=>$name));
-}
-
-echo (json_encode($myarray));
+        $categories= array();
+        $alldata = array();
+        $sameCategory = array();
+        $singleData= array();
+        while($row=mysqli_fetch_assoc($result)){
+            extract($row);
+            array_push($categories,array('id'=>$id,'category'=>$name));
+        }
+        
+        for($i=0;$i<sizeof($categories);$i++){
+            $query = "SELECT * FROM entities WHERE categoryId=".$categories[$i]['id'];
+        
+            $result = mysqli_query($this->connect,$query);
+             $sameCategory= array("category"=>$categories[$i]['category']);
+            while($row=mysqli_fetch_assoc($result)){
+                extract($row);
+                array_push($singleData,$row);
+                
+            }
+            $sameCategory['data']=$singleData;
+            if(sizeof($sameCategory['data'])>0){
+            array_push($alldata,$sameCategory);
+            }
+            
+            $singleData = array();
+        
+        }
+        print_r(json_encode($alldata));
     }
 }
 ?>
