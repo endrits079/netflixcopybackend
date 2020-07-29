2b75c5f39796e1fb3db83e8157bba0ee80b41dcd
Endrit Shabani <endrits079@gmail.com>
2020-07-29 12:19:06 +0200

    return false if movie not found

--- includes/classes/PreviewProvider.php
+++ includes/classes/PreviewProvider.php
@@ -16,9 +16,14 @@ class PreviewProvider{
         $query = "SELECT * from entities WHERE id =$id";
         $result = mysqli_query($this->connect,$query);
         $result = mysqli_fetch_assoc($result);
+        if(!empty($result)){
         extract($result);
         $data = array("image"=>$thumbnail,"video"=>$preview,"name"=>$name);
         echo json_encode($data);
+        }
+        else {
+            echo 'false';
+        }
 
     }
     private function getRandomEntity(){
